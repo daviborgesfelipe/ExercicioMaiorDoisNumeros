da0a72f00bad076d74622964a67068a53c06982d
Davi Borges <davifelipe_300@hotmail.com>
2022-01-24 21:17:50 -0300

    novo commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExercicioMaiorDoisNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first;
+            int second;
+
+            System.Console.Write("Digite o primeiro numero: ");
+            first = Convert.ToInt32(System.Console.ReadLine()); 
+
+            System.Console.Write("Digite o segundo numero: ");
+            second = Convert.ToInt32(System.Console.ReadLine());
+            
+            if(first > second)
+            {
+                System.Console.WriteLine($" {first} é maior que {second}!");
+            }
+            else if(first < second)
+            {
+                System.Console.WriteLine($" {second} é maior que {first}!");
+            }
+            else
+            {
+                System.Console.WriteLine($"{first} e {second} sao iguais!");
+            }
+        }
+    }
+}
